e697f31942455a2c827c5ca296c1cfb86b3cc36b
“JoDeLeon95” <JoDeLeon95@gmail.com>
2019-04-03 10:21:25 -0600

    Initial commit

---
+++ app/src/main/java/deepdive/cnm/edu/frontend_abqwifinder/controller/SearchResultFragment.java
@@ -0,0 +1,30 @@
+package deepdive.cnm.edu.frontend_abqwifinder.controller;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import deepdive.cnm.edu.frontend_abqwifinder.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchResultFragment extends Fragment {
+
+
+  public SearchResultFragment() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_search_result, container, false);
+  }
+
+}
